5e78b77aa2ff3ede7fff525ff77655e64b7ac1f8
Heltor <Heltor@192.168.1.18>
2022-01-09 14:52:58 +0100

    Squelette du code

--- Blockchain/src/Header.java
+++ Blockchain/src/Header.java
@@ -1,8 +1,42 @@
 
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
 public class Header {
-	private int blockid;
-	private int headerid;
-	private int bodyid;
+	private int blockId;
+	private int headerId;	
+	private int bodyId;
+	private int nonce;
+	private Date timeStamp = new Date();
+	private int blockTransHash;
+	private int headerHash;
+	private int headerHashPrev;
+	public Header() {
+		
+	};
+	public int getBlockId() {return blockId;}
+	
+	public int getNonce() {return nonce;}
+	
+	public void calcHeaderHash() {
+		
+	}
+	
+	public int getPrevHash() {return headerHashPrev;}
+	
+	public int getBlockTransHash() {return blockTransHash;}
+	
+	public int getBodyId( ) {return bodyId;}
+	
+	public void mineBlock() {
+		
+		
+	}
+	
+	
 	
 	
 }
