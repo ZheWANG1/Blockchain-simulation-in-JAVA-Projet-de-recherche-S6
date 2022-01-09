1729248c191d62439328387b5cdfad404a4c662a
yulin <eliane.shiyulin@gmail.com>
2022-01-09 14:40:24 +0100

    msg

--- Blockchain/src/Body.java
+++ Blockchain/src/Body.java
@@ -1,4 +1,25 @@
 
 public class Body {
+    private int bodyId;
+    private int headerId;
+    private String transaction;
+
+    public void body(int bodyId, int headerId, String transaction){
+        this.bodyId = bodyId;
+        this.headerId = headerId;
+        this.transaction = transaction;
+    }
+
+    public int getBodyId(){
+        return bodyId;
+    }
+
+    public int getHeaderId(){
+        return headerId;
+    }
+
+    public void setTransaction(String transaction){
+        this.transaction = transaction;
+    }
 
 }
