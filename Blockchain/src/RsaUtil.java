a332a8cc38c18b639f50ac36e581c3219f1dbb1a
Heltor <Heltor@LAPTOP-MRV05EN7.home>
2022-01-14 16:09:24 +0100

    AJOUT DE RSA UTIL AVEC LES FONCTIONS VERIFY SIGN ET GENERATEKEYPAIR

---
+++ Blockchain/src/RsaUtil.java
@@ -0,0 +1,63 @@
+import java.security.*;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+
+import java.util.Base64;
+
+public class RsaUtil {
+	public static KeyPair generateKeyPair() throws Exception {
+			KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+			generator.initialize(256);
+			KeyPair pair = generator.generateKeyPair();
+			return pair;
+	}
+	
+	public static String sign(String plainText, PrivateKey privateKey) throws Exception {
+        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        privateSignature.initSign(privateKey);
+        privateSignature.update(plainText.getBytes(StandardCharsets.UTF_8));
+
+        byte[] signature = privateSignature.sign();
+
+        return Base64.getEncoder().encodeToString(signature);
+    }
+
+    public static boolean verify(String plainText, String signature, PublicKey publicKey) throws Exception {
+        Signature publicSignature = Signature.getInstance("SHA256withRSA");
+        publicSignature.initVerify(publicKey);
+        publicSignature.update(plainText.getBytes(StandardCharsets.UTF_8));
+
+        byte[] signatureBytes = Base64.getDecoder().decode(signature);
+
+        return publicSignature.verify(signatureBytes);
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
