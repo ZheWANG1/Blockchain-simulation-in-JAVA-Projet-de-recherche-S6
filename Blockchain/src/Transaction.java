dc842f7a13a4c9998131b894042ca9b9b9f5f9ed
Heltor <Heltor@LAPTOP-MRV05EN7.home>
2022-01-15 14:48:26 +0100

    msg

--- Blockchain/src/Transaction.java
+++ Blockchain/src/Transaction.java
@@ -2,27 +2,12 @@ import java.security.*;
 
 public class Transaction {
     private final String transaction;
-    private final String transactionHash;
-    private final String fromID;
-    private final String toID;
+    private final int fromID;
+    private final int toID;
     private final int amount;
     private final long timeStamp;
     private final String signature;
     
-    public Body() {
-        this.transaction = transaction;
-    }
 
-    public String getTransaction() {
-        return transaction;
-    }
-
-    public void setTransaction(String transaction) {
-        this.transaction = transaction;
-    }
-
-    public String toString() {
-        return "Transaction info : " + transaction + "\n";
-    }
-
+  
 }
