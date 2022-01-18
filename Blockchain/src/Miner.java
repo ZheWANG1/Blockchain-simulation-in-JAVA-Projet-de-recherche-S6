1cc708f3d91afb52dc30551c1cd17841d2d903dd
Heltor <Heltor@LAPTOP-MRV05EN7.home>
2022-01-18 23:03:02 +0100

    Précisions

--- Blockchain/src/Miner.java
+++ Blockchain/src/Miner.java
@@ -7,5 +7,9 @@ public class Miner extends Node {
     }
 
     public void receiptTransaction(Transaction transaction){}
+    // Le mineur doit ajouter la transaction qui lui fais gagner de l'argent dans le block avant de miner
+    // Le mineur gagnant broadcastera le block avec la transaction de son gain sans verification de la signature (Pour simplifier)
+    // Le Network pendant le broadcast fera alors un update des wallets via la fonction : updateAllWallet
+    // Donc le miner gagnant aura juste a broadcaster son Block et les wallets seront update
     
 }
