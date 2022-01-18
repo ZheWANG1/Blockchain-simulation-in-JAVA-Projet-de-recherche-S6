471a8598e2d9bb7f800ccbb3078b1780914a2e75
Heltor <Heltor@LAPTOP-MRV05EN7.home>
2022-01-18 11:08:31 +0100

    update Node

--- Blockchain/src/Node.java
+++ Blockchain/src/Node.java
@@ -3,43 +3,24 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.List;
 
-public class Node implements Runnable {
+public class Node{
     private static final Object o = new Object();
     private static int cpt = 0;
     protected final int nodeId;
-    protected final String nom;
+    protected final String name;
     private Network network;
-    private List<Transaction> listTransaction;
 
-    public Node(String nom) {
+
+    public Node(String name) {
         synchronized (o) {
             this.nodeId = cpt++;
         }
-        this.nom = nom;
+        this.name = name;
 
     }
 
-    public Node(String nom, Network network) {
-        this.nom = nom;
+    public Node(String name, Network network) {
+        this(name);
         this.network = network;
     }
-
-    public void getBlock(){}
-
-    public boolean verifySign() {
-        return true;
-    }
-
-    public int getNodeId(){ return nodeId;}
-
-
-    public void addTransaction(Transaction transaction){
-        listTransaction.add(transaction);
-    }
-
-    @Override
-    public void run() {
-
-    }
-
 }
