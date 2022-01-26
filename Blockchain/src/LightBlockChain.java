50a77260fd787f39ab8dd5c1135ca9f3de2c51da
Zhe WANG <zhe.wang@etu.sorbonne-universite.fr>
2022-01-26 23:13:05 +0100

    doc LightBlockChain

--- Blockchain/src/LightBlockChain.java
+++ Blockchain/src/LightBlockChain.java
@@ -1,27 +1,28 @@
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Objects;
 
-public class LightBlockChain extends Blockchain{
-	private static int cpt = 0;
-	private final int lightBlockID;
-	private final ArrayList<LightBlock> LightBlkchain = new ArrayList<LightBlock>();
+/**
+ * A light blockchain class used by transaction nodes that keeps only the ten most recent blockheads.
+ */
+public class LightBlockChain extends Blockchain {
+    private final ArrayList<LightBlock> LightBlkchain = new ArrayList<>();
 
-	public LightBlockChain() {
-		super();
-		lightBlockID = cpt;
-		cpt++;
-	}
+    public LightBlockChain() {
+        super();
+    }
 
-	public void addLightHeader(Header h) {
-		String headerHash = h.getHeaderHash();
-		String previousHeaderHash = h.getPrevHash();
-		String trsHash = h.getBlockTransHash();
-		LightBlock ln = new LightBlock(previousHeaderHash, headerHash, trsHash);
-		LightBlkchain.add(ln);
-		if (LightBlkchain.size() == 11) {
-			LightBlkchain.remove(0);
-		}
-	}
+    /**
+     * Add new light block to this light blockchain
+     * @param h Header of the new block
+     */
+    public void addLightHeader(Header h) {
+        String headerHash = h.getHeaderHash();
+        String previousHeaderHash = h.getPrevHash();
+        String trsHash = h.getBlockTransHash();
+        LightBlock ln = new LightBlock(previousHeaderHash, headerHash, trsHash);
+        LightBlkchain.add(ln);
+        while (LightBlkchain.size() >= 11) {
+            LightBlkchain.remove(0);
+        }
+    }
 
 }
